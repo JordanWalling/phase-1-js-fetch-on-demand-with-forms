61558e4def742729fe8bedbd2d4071c812a83f5f
JordanWalling <jordanwalling1991@gmail.com>
2024-01-16 21:01:58 +1100

    complete fetch with forms lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  formSubmit();
+};
+
+function formSubmit() {
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputValue = document.querySelector("input#searchByID").value;
+    console.log(inputValue);
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
